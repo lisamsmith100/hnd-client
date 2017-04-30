c2404700168f3aeff988eca3a9f2c01c839091d3
lisamsmith100 <lisamarie.smith@libertymutual.com>
2017-04-30 12:48:00 -0400

    Add file for get entries (show)
    This is working but create is not yet working

--- scripts/get-entries.sh
+++ scripts/get-entries.sh
@@ -1,1 +1,13 @@
-curl --include --request GET https://hnd-api.herokuapp.com/entries
+#curl --include --request GET https://hnd-api.herokuapp.com/entries
+#!/bin/bash
+#TOKEN=BAhJIiU2NzdjM2E4MmExNTgwMThlNDM4ZjlmNDc2NjhmZTNjMwY6BkVG--28254e556274f03845295dd66d24e92c53a4e4dc sh scripts/get-entries.sh
+#curl --include --request GET http://localhost:4741/entries
+
+API="${API_ORIGIN:-https://hnd-api.herokuapp.com}"
+URL_PATH="/entries"
+curl "${API}${URL_PATH}" \
+  --include \
+  --request GET \
+  --header "Authorization: Token token=$TOKEN"
+
+echo
